506bdb53ed9337768cc43d796bb7686fd0421e63
nexryai <61890205+nexryai@users.noreply.github.com>
2025-11-04 03:34:41 +0000

    WIP

--- internal/worker/job.go
+++ internal/worker/job.go
@@ -1,23 +1,39 @@
 package worker
 
 import (
+	"context"
 	"fmt"
+	"os"
+
+	"github.com/nexryai/eleos/internal/db"
+)
+
+var (
+	dbConnectString = os.Getenv("DB_CONNECT_STRING")
 )
 
 func ExecuteJob() error {
-	vulnerabilities, err := fetchNewVulnerabilities()
+	nvdVulnerabilities, err := fetchNewVulnerabilities()
 	if err != nil {
 		return fmt.Errorf("error executing job: %w", err)
 	}
 
-	if vulnerabilities == nil {
+	if nvdVulnerabilities == nil {
 		// No vulnerabilities found, nothing to process
 		return nil
 	}
 
-	_, err = processVulnerabilities(vulnerabilities)
+	vulnerabilities, err := processVulnerabilities(nvdVulnerabilities)
 	if err != nil {
 		return fmt.Errorf("error processing vulnerabilities: %w", err)
+	}
+
+	dbContext := context.Background()
+	database, err := db.NewDBConn(dbContext, dbConnectString)
+
+	db.CreateVulnerabilityBatch(dbContext, database, vulnerabilities)
+	if err != nil {
+		return fmt.Errorf("error connecting to database: %w", err)
 	}
 
 	return nil
