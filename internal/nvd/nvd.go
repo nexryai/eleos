15a56a71f7f972f43b6b55c2bbe2781b249303f7
nexryai <61890205+nexryai@users.noreply.github.com>
2025-11-04 03:43:14 +0000

    nvd: 複数のバージョンのCVSSをサポート

--- internal/nvd/nvd.go
+++ internal/nvd/nvd.go
@@ -51,17 +51,17 @@ type VulnerabilityItem struct {
 }
 
 type CVE struct {
-	ID               string          `json:"id"`
-	SourceIdentifier string          `json:"sourceIdentifier"`
-	Published        NVRTime         `json:"published"`
-	LastModified     NVRTime         `json:"lastModified"`
-	VulnStatus       string          `json:"vulnStatus"`
+	ID               string  `json:"id"`
+	SourceIdentifier string  `json:"sourceIdentifier"`
+	Published        NVRTime `json:"published"`
+	LastModified     NVRTime `json:"lastModified"`
+	VulnStatus       string  `json:"vulnStatus"`
 	//CveTags          []string        `json:"cveTags"`
-	Descriptions     []Description   `json:"descriptions"`
-	Metrics          Metrics         `json:"metrics"`
-	Weaknesses       []Weakness      `json:"weaknesses"`
-	Configurations   []Configuration `json:"configurations"`
-	References       []Reference     `json:"references"`
+	Descriptions   []Description   `json:"descriptions"`
+	Metrics        Metrics         `json:"metrics"`
+	Weaknesses     []Weakness      `json:"weaknesses"`
+	Configurations []Configuration `json:"configurations"`
+	References     []Reference     `json:"references"`
 }
 
 type Description struct {
@@ -70,7 +70,9 @@ type Description struct {
 }
 
 type Metrics struct {
-	CVSSMetricV31 []CVSSMetricV31 `json:"cvssMetricV31"`
+	CVSSMetricV40 []CVSSMetricV40 `json:"cvssMetricV40,omitempty"`
+	CVSSMetricV31 []CVSSMetricV31 `json:"cvssMetricV31,omitempty"`
+	CVSSMetricV2  []CVSSMetricV2  `json:"cvssMetricV2,omitempty"`
 }
 
 type CVSSMetricV31 struct {
@@ -79,6 +81,79 @@ type CVSSMetricV31 struct {
 	CVSSData            CVSSDataV31 `json:"cvssData"`
 	ExploitabilityScore float64     `json:"exploitabilityScore"`
 	ImpactScore         float64     `json:"impactScore"`
+}
+
+// CVSSMetricV40 と CVSSDataV40 は NVD の cvssMetricV40 をパースするための型です
+type CVSSMetricV40 struct {
+	Source   string      `json:"source"`
+	Type     string      `json:"type"`
+	CVSSData CVSSDataV40 `json:"cvssData"`
+}
+
+type CVSSDataV40 struct {
+	Version                           string  `json:"version"`
+	VectorString                      string  `json:"vectorString"`
+	BaseScore                         float64 `json:"baseScore"`
+	BaseSeverity                      string  `json:"baseSeverity"`
+	AttackVector                      string  `json:"attackVector"`
+	AttackComplexity                  string  `json:"attackComplexity"`
+	AttackRequirements                string  `json:"attackRequirements"`
+	PrivilegesRequired                string  `json:"privilegesRequired"`
+	UserInteraction                   string  `json:"userInteraction"`
+	VulnConfidentialityImpact         string  `json:"vulnConfidentialityImpact"`
+	VulnIntegrityImpact               string  `json:"vulnIntegrityImpact"`
+	VulnAvailabilityImpact            string  `json:"vulnAvailabilityImpact"`
+	SubConfidentialityImpact          string  `json:"subConfidentialityImpact"`
+	SubIntegrityImpact                string  `json:"subIntegrityImpact"`
+	SubAvailabilityImpact             string  `json:"subAvailabilityImpact"`
+	ExploitMaturity                   string  `json:"exploitMaturity"`
+	ConfidentialityRequirement        string  `json:"confidentialityRequirement"`
+	IntegrityRequirement              string  `json:"integrityRequirement"`
+	AvailabilityRequirement           string  `json:"availabilityRequirement"`
+	ModifiedAttackVector              string  `json:"modifiedAttackVector"`
+	ModifiedAttackComplexity          string  `json:"modifiedAttackComplexity"`
+	ModifiedAttackRequirements        string  `json:"modifiedAttackRequirements"`
+	ModifiedPrivilegesRequired        string  `json:"modifiedPrivilegesRequired"`
+	ModifiedUserInteraction           string  `json:"modifiedUserInteraction"`
+	ModifiedVulnConfidentialityImpact string  `json:"modifiedVulnConfidentialityImpact"`
+	ModifiedVulnIntegrityImpact       string  `json:"modifiedVulnIntegrityImpact"`
+	ModifiedVulnAvailabilityImpact    string  `json:"modifiedVulnAvailabilityImpact"`
+	ModifiedSubConfidentialityImpact  string  `json:"modifiedSubConfidentialityImpact"`
+	ModifiedSubIntegrityImpact        string  `json:"modifiedSubIntegrityImpact"`
+	ModifiedSubAvailabilityImpact     string  `json:"modifiedSubAvailabilityImpact"`
+	Safety                            string  `json:"Safety"`
+	Automatable                       string  `json:"Automatable"`
+	Recovery                          string  `json:"Recovery"`
+	ValueDensity                      string  `json:"valueDensity"`
+	VulnerabilityResponseEffort       string  `json:"vulnerabilityResponseEffort"`
+	ProviderUrgency                   string  `json:"providerUrgency"`
+}
+
+// CVSSMetricV2 と CVSSDataV20 は NVD の cvssMetricV2 をパースするための型です
+type CVSSMetricV2 struct {
+	Source                  string      `json:"source"`
+	Type                    string      `json:"type"`
+	CVSSData                CVSSDataV20 `json:"cvssData"`
+	BaseSeverity            string      `json:"baseSeverity,omitempty"`
+	ExploitabilityScore     float64     `json:"exploitabilityScore,omitempty"`
+	ImpactScore             float64     `json:"impactScore,omitempty"`
+	AcInsufInfo             bool        `json:"acInsufInfo,omitempty"`
+	ObtainAllPrivilege      bool        `json:"obtainAllPrivilege,omitempty"`
+	ObtainUserPrivilege     bool        `json:"obtainUserPrivilege,omitempty"`
+	ObtainOtherPrivilege    bool        `json:"obtainOtherPrivilege,omitempty"`
+	UserInteractionRequired bool        `json:"userInteractionRequired,omitempty"`
+}
+
+type CVSSDataV20 struct {
+	Version               string  `json:"version"`
+	VectorString          string  `json:"vectorString"`
+	BaseScore             float64 `json:"baseScore"`
+	AccessVector          string  `json:"accessVector"`
+	AccessComplexity      string  `json:"accessComplexity"`
+	Authentication        string  `json:"authentication"`
+	ConfidentialityImpact string  `json:"confidentialityImpact"`
+	IntegrityImpact       string  `json:"integrityImpact"`
+	AvailabilityImpact    string  `json:"availabilityImpact"`
 }
 
 type CVSSDataV31 struct {
