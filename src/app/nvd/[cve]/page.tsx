8737bfe1f5e85e31a6321562fb725887d15b5f10
nexryai <61890205+nexryai@users.noreply.github.com>
2025-11-10 10:46:44 +0000

    WIP

--- src/app/nvd/[cve]/page.tsx
+++ src/app/nvd/[cve]/page.tsx
@@ -1,10 +1,14 @@
+import { test } from "@/actions/cve";
+
 export default async ({ params }: { params: Promise<{ cve: string }> }) => {
     const cveId = (await params).cve;
+
+    const testString = test();
 
     return (
         <>
             <h1 className="text-2xl">{cveId}</h1>
-            <p>test</p>
+            <p>test {testString}</p>
         </>
     );
 };
