cbcda00ede0f885ab87928245dff84d0df0e77c8
nexryai <61890205+nexryai@users.noreply.github.com>
2025-11-28 07:02:47 +0000

    Remove bg color

--- src/app/nvd/[cve]/page.tsx
+++ src/app/nvd/[cve]/page.tsx
@@ -16,9 +16,6 @@ export default async function ({ params }: { params: Promise<{ cve: string }> })
                     {data.cvss ? (
                         <div className="flex gap-4 flex-col justify-end relative">
                             <span>CVSS {data.cvss.version}</span>
-                            <div className="absolute top-0 -left-18 -z-10">
-                                <div className="w-48 h-48 bg-linear-to-br from-red-50 to-red-200 blur-3xl"></div>
-                            </div>
                             <div className="flex gap-4 items-center text-3xl text-right text-red-600 rounded-full p-2">
                                 <IconShieldExclamation className="ml-4" />
                                 <span className="mr-4">{data.cvss.score}</span>
