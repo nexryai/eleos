d990120901e2920d2ab6972e93226e2cdc710b89
nexryai <61890205+nexryai@users.noreply.github.com>
2025-12-16 03:40:57 +0000

    Fix

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -1,27 +1,23 @@
 resource "google_cloud_run_v2_job" "default" {
-  name     = "update-db-job"
+  name     = "db-update-job"
   location = var.region
 
   template {
+    task_count  = 1
+    # parallelism = 1
+
     template {
+      max_retries = 0
+
       containers {
         image = var.image_url
-        
         resources {
           limits = {
             cpu    = "1000m"
             memory = "512Mi"
           }
         }
-        
-        env {
-          name  = "ENV_VAR"
-          value = "value"
-        }
       }
-      
-      max_retries = 0 
-      task_count  = 1
     }
   }
 }
