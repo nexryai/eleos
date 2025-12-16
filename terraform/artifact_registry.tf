d8ebb144a7c51e2265fdb941ddb5ea60213d413b
nexryai <61890205+nexryai@users.noreply.github.com>
2025-12-16 05:12:00 +0000

    Terraform: 不要なイメージは即削除するように

--- terraform/artifact_registry.tf
+++ terraform/artifact_registry.tf
@@ -8,16 +8,15 @@ resource "google_artifact_registry_repository" "repo" {
     id     = "keep-minimum-versions"
     action = "KEEP"
     most_recent_versions {
-      keep_count = 3 # 最新の3つだけ残す
+      keep_count = 2
     }
   }
 
   cleanup_policies {
-    id     = "delete-old-images"
+    id     = "delete-unprotected-images"
     action = "DELETE"
     condition {
-      # 上記KEEPルールにマッチしない、かつアップロードから一定時間経過したものを削除
-      older_than = "86400s" # 1日以上経過したもの
+      older_than = "0s"
     }
   }
 }
